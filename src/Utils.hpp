7eee25fedcd657462c3f06fb5d7c6215bc7a200f
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-02-21 23:40:38 +0700

    feat: implement choice display functionality for weapon selection in shop

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -1,8 +1,21 @@
 #pragma once
 #include <string>
+#include <vector>
 #include "Weapon.hpp"
 #include "Armor.hpp"
+#include <conio.h>
+#include <iomanip>
+#include "Constants.hpp"
+
+using namespace Constants::Frame;
 
 WeaponType stringToWeaponType(const std::string &typeStr);
 ArmorType stringToArmorType(const std::string &typeStr);
 ArmorPart stringToArmorPart(const std::string &partStr);
+
+namespace Choice {
+    void clearLastTwoLines();
+    void clearLines(int n);
+    void display(std::string name, int option);
+    bool show(std::string name = "");
+}
