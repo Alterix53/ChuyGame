640b9eec1e6413417793a10afe2ec90d60e8b1a6
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-02-25 19:41:19 +0700

    refactor: enhance displayInventory functionality

--- src/GameFlow/Inventory.hpp
+++ src/GameFlow/Inventory.hpp
@@ -15,10 +15,9 @@ const int itemsPerCol = 4;
 using namespace utils::dialog;
 
 namespace Inventory {
-    extern std::vector<std::string> items;
+    // extern std::vector<std::string> items;
 
-    void displayInventory(Player &player);
-    void addItem(const std::string& item);
+    // void addItem(const std::string& item);
     // void addWeapon(Player &player, const Weapon& weapon);
     // void addArmor(Player &player, const Armor& armor);
     // void removeWeapon(Player &player, const Weapon& weapon);
@@ -27,8 +26,11 @@ namespace Inventory {
     // bool hasArmor(Player &player, const Armor& armor);
     void show(Player &player);
     void compareWeapon(Player &player, Weapon w1, Weapon w2);
-    void compareArmor(Player &player, Armor a1, Armor a2);   
-    // void listWeapons(Player &player);
+    void compareArmor(Player &player, Armor a1, Armor a2); 
+    std::vector<std::string> splitLines(const std::string& text);
+    void displayInventory(std::vector<Item*> &items, Player &player, std::pair<int, int> inventoryPos, int pageNumber);
+    void drawGrid(const std::vector<Item*>& data, int cols = 4, int rows = 4, int pageNumber = 1);
+        // void listWeapons(Player &player);
     // void listArmors(Player &player);
     
 }
